448fdedab59367875f7a6157f4b530e9ea15e86c
Dave Brophy <dave@brophy.uk>
2017-10-29 08:45:43 -0700

    Fixed slow resuming

--- loop-main.go
+++ loop-main.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 	"io"
 
+	"encoding/json"
+
+	"sync/atomic"
+
+	"github.com/leemcloughlin/gofarmhash"
 	"github.com/pkg/errors"
 )
 
@@ -41,7 +46,47 @@ func (b *Blaster) startMainLoop(ctx context.Context) {
 						b.error(errors.WithStack(err))
 						return
 					}
-					b.workerChannel <- workDef{Record: record}
+
+					skipped := true
+
+					for _, payloadVariantData := range b.config.PayloadVariants {
+
+						// Build the full data map that will be passed to the worker
+						data := map[string]string{}
+						for i, k := range b.dataHeaders {
+							// Add data from the CSV data source
+							data[k] = record[i]
+						}
+						for k, v := range payloadVariantData {
+							// Add data from the payload-variants config
+							data[k] = v
+						}
+
+						// Calculate the hash of the incoming data
+						j, err := json.Marshal(data)
+						if err != nil {
+							b.error(errors.WithStack(err))
+							return
+						}
+						hash := farmhash.Hash128(j)
+
+						// In resume mode, check to see if the hash occurred in a previous run
+						// (skip only contains successful requests from previous runs).
+						if b.config.Resume {
+							if _, skip := b.skip[hash]; skip {
+								atomic.AddUint64(&b.stats.requestsSkipped, 1)
+								continue
+							}
+						}
+
+						skipped = false
+
+						b.workerChannel <- workDef{Data: data, Hash: hash}
+					}
+					if skipped {
+						// if we've skipped all variants, continue with the next item immediately
+						continue
+					}
 					break
 				}
 			}
@@ -50,5 +95,6 @@ func (b *Blaster) startMainLoop(ctx context.Context) {
 }
 
 type workDef struct {
-	Record []string
+	Data map[string]string
+	Hash farmhash.Uint128
 }
